4ddff42b3adffd94b306a2796e11bb7e10cdefc1
briankim02121997 <briankim02121997@gmail.com>
2021-12-23 20:50:31 +0700

    Flatten all contract

--- tokens/SCE.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.9;
-
-import "./core/TokenBase.sol";
-
-contract SCE is TokenBase {
-    constructor(address multiSigAccount) TokenBase(multiSigAccount, "SCE", "SCE") {}
-}
